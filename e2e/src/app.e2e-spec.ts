0aeaa2b31698e29a811fbb276463196d4c3c0c05
CM-haruna-oka <oka.haruna@classmethod.jp>
2020-01-05 13:31:09 +0900

    プロジェクト名変更

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-trello-mock app is running!');
+    expect(page.getTitleText()).toEqual('my-todo-app app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-trello-mock'`, () => {
+  it(`should have as title 'my-todo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-trello-mock');
+    expect(app.title).toEqual('my-todo-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-trello-mock app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('my-todo-app app is running!');
   });
 });
